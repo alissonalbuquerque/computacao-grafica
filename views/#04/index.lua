572f92ea0d0b07ca3ca6f0142a9d255c26ed73fd
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2024-11-29 12:23:02 -0300

    alteração de audio e/ou views na página #03 #04

--- views/#04/index.lua
+++ views/#04/index.lua
@@ -50,10 +50,10 @@ function scene:create(event)
     page_image.x = Dimension.centerX
     page_image.y = Dimension.centerY
 
-    btn_audio_off = AudioOff.create({ scene_group = sceneGroup })
     btn_audio_on  = AudioOn.create({ scene_group = sceneGroup })
+    btn_audio_off = AudioOff.create({ scene_group = sceneGroup })
 
-    -- audio_player = AudioPlayer.new({path_audio_file = 'views/#04/audio/audio.mp3'})
+    audio_player = AudioPlayer.new({path_audio_file = 'views/#04/audio/audio.mp3'})
 
     local btn_back = ButtonBackPage.create({ scene_group = sceneGroup, path_back_page = 'views.#03.index', audio_player = audio_player, params = params })
     local btn_next = ButtonNextPage.create({ scene_group = sceneGroup, path_next_page = 'views.#05.index', audio_player = audio_player, params = params })
@@ -63,7 +63,7 @@ function scene:create(event)
         local _btn_audio_on = event.target
         local _btn_audio_off = btn_audio_off
 
-        -- local _audio_player = audio_player
+        local _audio_player = audio_player
 
         _btn_audio_on.isVisible = false;
         _btn_audio_on.isEnabled = false;
@@ -71,7 +71,7 @@ function scene:create(event)
         _btn_audio_off.isVisible = true;
         _btn_audio_off.isEnabled = true;
 
-        -- _audio_player:volumeOff()
+        _audio_player:volumeOff()
     end)
 
     btn_audio_off:addEventListener("touch", function (event) 
@@ -79,7 +79,7 @@ function scene:create(event)
         local _btn_audio_on = btn_audio_on
         local _btn_audio_off = event.target
 
-        -- local _audio_player = audio_player
+        local _audio_player = audio_player
 
         _btn_audio_on.isVisible = true
         _btn_audio_on.isEnabled = true
@@ -87,7 +87,7 @@ function scene:create(event)
         _btn_audio_off.isVisible = false
         _btn_audio_off.isEnabled = false
 
-        -- _audio_player:volumeOn()
+        _audio_player:volumeOn()
     end)
 
 end
@@ -99,13 +99,13 @@ function scene:show(event)
     local phase = event.phase
 
     if (phase == "will") then
-        -- Inicialize objetos e faça transições antes de mostrar a cena
-        -- audio_player:volumeOff()
-        -- audio_player:stop()
+        
+        audio_player:volumeOff()
+        audio_player:stop()
 
     elseif (phase == "did") then
-        -- Lógica para quando a cena é mostrada
-        -- audio_player:play()
+        
+        audio_player:play()
     end
 end
 
