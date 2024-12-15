50faa74a4f79168d6eaf6ab690db44ab25065cd0
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2024-12-14 22:50:14 -0300

    feat(Page6): Add audio e texto novo para o projeto

--- views/Page6/index.lua
+++ views/Page6/index.lua
@@ -53,7 +53,7 @@ function scene:create(event)
     btn_audio_off = AudioOff.create({ scene_group = sceneGroup })
     btn_audio_on  = AudioOn.create({ scene_group = sceneGroup })
 
-    -- local audio_player = AudioPlayer.new({path_audio_file = 'resources/audio/page01/audio.mp3'})
+    audio_player = AudioPlayer.new({path_audio_file = 'views/Page6/audio/audio.mp3'})
 
     local btn_back = ButtonBackPage.create({ scene_group = sceneGroup, path_back_page = 'views.Page5.index', audio_player = audio_player })
     local btn_next = ButtonNextPage.create({ scene_group = sceneGroup, path_next_page = 'views.Page7.index', audio_player = audio_player })
@@ -63,7 +63,7 @@ function scene:create(event)
         local _btn_audio_on = event.target
         local _btn_audio_off = btn_audio_off
 
-        -- local _audio_player = audio_player
+        local _audio_player = audio_player
 
         _btn_audio_on.isVisible = false;
         _btn_audio_on.isEnabled = false;
@@ -71,7 +71,7 @@ function scene:create(event)
         _btn_audio_off.isVisible = true;
         _btn_audio_off.isEnabled = true;
 
-        -- _audio_player:volumeOff()
+        _audio_player:volumeOff()
     end)
 
     btn_audio_off:addEventListener("touch", function (event) 
@@ -79,7 +79,7 @@ function scene:create(event)
         local _btn_audio_on = btn_audio_on
         local _btn_audio_off = event.target
 
-        -- local _audio_player = audio_player
+        local _audio_player = audio_player
 
         _btn_audio_on.isVisible = true
         _btn_audio_on.isEnabled = true
@@ -87,20 +87,11 @@ function scene:create(event)
         _btn_audio_off.isVisible = false
         _btn_audio_off.isEnabled = false
 
-        -- _audio_player:volumeOn()
+        _audio_player:volumeOn()
     end)
 
-    if not params['audio_enable'] then
-
-        btn_audio_on:dispatchEvent({
-            name = 'touch',
-            phase = 'ended',
-            x = btn_audio_on.x,
-            y = btn_audio_on.y,
-            target = btn_audio_on
-        })
-
-    end
+    -- INTERATIONS IN VIEW --
+    -----------------------------------------------------------------------------------------
     
 end
 
@@ -111,9 +102,19 @@ function scene:show(event)
     local phase = event.phase
 
     if (phase == "will") then
-        -- Inicialize objetos e faça transições antes de mostrar a cena
+        
+        btn_audio_on.isVisible = false
+        btn_audio_on.isEnabled = false
+
+        btn_audio_off.isVisible = true
+        btn_audio_off.isEnabled = true
+
+        audio_player:volumeOff()
+        audio_player:stop()
+
     elseif (phase == "did") then
-        -- Lógica para quando a cena é mostrada
+
+        audio_player:play()
     end
 end
 
@@ -124,9 +125,9 @@ function scene:hide(event)
     local phase = event.phase
 
     if (phase == "will") then
-        -- Faça preparativos antes de ocultar a cena
+        
     elseif (phase == "did") then
-        -- Limpeza após a cena ser ocultada
+
     end
 end
 
@@ -134,7 +135,6 @@ end
 -- @return void
 function scene:destroy(event)
     local sceneGroup = self.view
-    -- Limpe recursos quando a cena for destruída
 end
 
 -- Adicione os ouvintes de cena
